e41c77aeedabc8e7d6222bd4621d8be62a12cb93
Max <max2410zp@gmail.com>
2019-02-06 21:33:28 +0200

    ...

--- js/skillbar.js
+++ js/skillbar.js
@@ -1,58 +1,63 @@
-const circlesArray = [
-  document.getElementById('circle1').getContext('2d'),
-  document.getElementById('circle2').getContext('2d'),
-  document.getElementById('circle3').getContext('2d'),
-//  document.getElementById('circle4').getContext('2d'),
-//  document.getElementById('circle5').getContext('2d'),
-//  document.getElementById('circle6').getContext('2d'),
-//  document.getElementById('circle7').getContext('2d'),
-//  document.getElementById('circle8').getContext('2d'),
-//  document.getElementById('circle9').getContext('2d'),
-//  document.getElementById('circle10').getContext('2d'),
-//  document.getElementById('circle11').getContext('2d')
-];
-//***SKILLS(id) : VALUE(%)***//
+////////////////////////////////
+//***SkillName : VALUE(%)***//
 const skillsArray = {
   HTML: 0,
   CSS: 0,
   JavaScript: 0,
-//  jQuery: 0,
-//  Bootstrap: 0,
-//  Sass: 0,
-//  'React-Redux': 0,
-//  NodeJS: 0,
-//  PHP: 0,
-//  Python: 0,
-//  SQL: 0
+ jQuery: 0,
+ Bootstrap: 0,
+ Sass: 0,
+ 'React-Redux': 0,
+ NodeJS: 0,
+ PHP: 0,
+ MySQL: 0,
+ Python: 0
 };
-for (let i = 0; i < circlesArray.length; i++) {
-  circlesArray[i].lineWidth = 3; //thickness of the line
-  circlesArray[i].fillStyle = 'rgb(192, 192, 192, 0.3)';
-  circlesArray[i].strokeStyle = "rgb(192, 192, 192, 0.3)";
-  circlesArray[i].beginPath();
-  circlesArray[i].arc(55, 30, 20, 4.72, 15, false); //.arc(x, y , radius, startAngle, endAngle, anticlockwise)
-  circlesArray[i].stroke();
+////////////////////////////////
+
+const barsArray = [];
+
+function addSkills(barNumber, skillName) {
+  let newSkill = document.createElement('div');
+  newSkill.className = "col barDiv";
+  newSkill.innerHTML = `<canvas id= ${barNumber} className="skillBar" width="110" height="80"></canvas>
+                        <canvas id= ${skillName} width="110" height="80" className="skillName "></canvas>`;
+  document.getElementById('skills').appendChild(newSkill);
+
+  barsArray.push(document.getElementById(barNumber).getContext('2d'));
+}
+
+for (let i = 0; i < barsArray.length; i++) {
+  barsArray[i].lineWidth = 3; //thickness of the line
+  barsArray[i].fillStyle = 'rgb(192, 192, 192, 0.3)';
+  barsArray[i].strokeStyle = 'rgb(192, 192, 192, 0.3)';
+  barsArray[i].beginPath();
+  barsArray[i].arc(55, 30, 20, 4.72, 15, false); //.arc(x, y , radius, startAngle, endAngle, anticlockwise)
+  barsArray[i].stroke();
 }
 
 window.onload = () => {
-    let id;
+    let skillName;
     let value;
+    let counter = 0;
     for(skill in skillsArray) {
-      id = skill;
+      skillName = skill;
       value = skillsArray[skill];
-      loadSkills(id, value);
+      ++counter;
+      barNumber = 'bar' + counter;
+      addSkills(barNumber, skillName)
+      loadSkills(skillName, value);
     }
 }
 
-//***Function LoadSkills***//
-function loadSkills(id, value) {
-  let ctx = document.getElementById(id).getContext('2d');
+function loadSkills(skillName, value) {
+  let ctx = document.getElementById(skillName).getContext('2d');
   let al = 0;
   let start = 4.72;
   let cw = ctx.canvas.width;
   let ch = ctx.canvas.height;
   let diff;
-  let name = id.replace('-', ' / ');
+  let name = skillName.replace('-', ' / ');
 
   let progressSim = () => {
     diff = ((al / 100) * Math.PI * 2 * 10).toFixed(2);
